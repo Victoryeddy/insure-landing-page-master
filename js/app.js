e46bb225f12acb635585c8d31dd84d3f434c9015
Victory Ndu <victor5ndu@gmail.com>
2022-08-29 16:01:54 +0100

    i created this insurance landing page

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const navToggler = document.querySelector(".nav-toggler");
+const navBody = document.querySelector(".nav-body");
+const hamburgerIcon = document.querySelector(".hamburger-icon");
+const closeIcon = document.querySelector(".close-icon");
+
+navToggler.addEventListener("click", () => {
+    navBody.classList.toggle("show-navBody")
+
+
+
+})
